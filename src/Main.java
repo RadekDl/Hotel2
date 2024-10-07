06fa8674b5acf1dc1fde9ec77b433fec5501f962
RadekDl <radek.dl@atlas.cz>
2024-10-07 13:57:22 +0200

    DÚ
    přidání výpisu rezervací pomocí ciklu for

--- src/Main.java
+++ src/Main.java
@@ -39,18 +39,33 @@ public class Main {
         Booking booking1 = new Booking(LocalDate.of(2021,7,1),LocalDate.of(
                 2021,7,19),room1,List.of(customer1),true);
         Booking booking2 = new Booking(LocalDate.of(2021,9,1),LocalDate.of(
-                2021,9,14),room3,List.of(customer1,customer2),false);                                                                   // jak tam zapsat customer1 a customer2
+                2021,9,14),room3,List.of(customer1,customer2),false);
 
 
-        System.out.println("Rezervace pokoje číslo "+ room1.getRoomOfNumber()+" je od "+
+
+
+        System.out.println("\n Rezervace pokoje číslo "+ room1.getRoomOfNumber()+" je od "+
                             booking1.getReservationOn()+" do "+ booking1.getReservationOff()+
                             " klientem, "+ booking1.getCustomer().getFirst().getSecondName()+
                             "\n a pobyt je pracovní "+ booking1.isStay());
 
-        System.out.println("Rezervace pokoje číslo "+room3.getRoomOfNumber()+" je "+ "od "
+        System.out.println("\n Rezervace pokoje číslo "+room3.getRoomOfNumber()+" je "+ "od "
                 +booking2.getReservationOn()+" do "+booking2.getReservationOff()+" klientem/ty"+","+" "
                 + booking2.getCustomer().getFirst().getSecondName()+", "+
-                booking2.getCustomer().get(1).getSecondName()+" a pobyt je pracovní "+booking2.isStay());
+                booking2.getCustomer().get(1).getSecondName()+" a pobyt je pracovní "+booking2.isStay()+"\n");
+
+
+        List<Booking> bookings = new ArrayList<>();
+        bookings.add(booking1);
+        bookings.add(booking2);
+
+        for (Booking bookingPrint : bookings) {
+            System.out.println("Začátek rezervace, "+bookingPrint.getReservationOn()+
+                    "\n Konec rezervace, "+ bookingPrint.getReservationOff()+"\n zákazníkem, "+
+                    bookingPrint.getCustomer());
+        }
+
+
 
     }
 }
